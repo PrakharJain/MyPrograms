d4cb54b80076603d26bbefa79da946b9207d9fc3
Swati <swati@swati-VirtualBox.(none)>
2013-02-26 19:18:25 +0530

    Added code to print the graph

--- DataStructures/graphs/graphAL.cpp
+++ DataStructures/graphs/graphAL.cpp
@@ -3,21 +3,35 @@
 
 void GraphAL::addEdge(string& id1 , string& id2)
 {
-	boost::shared_ptr<Vertex> & node1 = nodes[id1];
-	boost::shared_ptr<Vertex>& node2 = nodes[id2];
+	nodeptr& node1 = nodes[id1];
+	nodeptr& node2 = nodes[id2];
 	node1->neighbours.push_back(node2);
 	node2->neighbours.push_back(node1);	
 }
 
 
-const vector<boost::shared_ptr <Vertex> >& GraphAL::getNeighbours(string &id)
+const vector< nodeptr >& GraphAL::getNeighbours(string &id)
 {
 	boost::shared_ptr<Vertex>&node = nodes[id];
 	return node->neighbours;
 }
 
 
-const map <string , boost::shared_ptr<Vertex> > & GraphAL::getNodes()
+const map <string , nodeptr > & GraphAL::getNodes()
 {
 	return nodes;
 }
+
+void GraphAL::printGraph()
+{
+	for( map<string , nodeptr >::iterator it = nodes.begin(); it != nodes.end() ; it++)
+	{
+		cout << "Node " << it->first << "is conected to ";
+		vector< nodeptr >& neighbours = it->second->neighbours;
+		for(vector< nodeptr >::iterator n = neighbours.begin() ; n!= neighbours.end() ; n++)
+		{
+		cout << (*n)->id << " " ;
+		}
+	}
+}
+
--- DataStructures/graphs/graphAL.h
+++ DataStructures/graphs/graphAL.h
@@ -6,11 +6,13 @@
 #include <map>
 #include <boost/shared_ptr.hpp>
 using namespace std;
+struct Vertex;
+typedef boost::shared_ptr<Vertex> nodeptr;
 
 struct Vertex
 {
 	string id;
-	vector< boost::shared_ptr<Vertex> > neighbours;
+	vector< nodeptr > neighbours;
 	Vertex() 
 	{
 
@@ -25,14 +27,15 @@ struct Vertex
 class GraphAL
 {
 	public:
-	void addEdge(string &id1  , string& id2);
-	void BFS();
-	void DFS();
-	~GraphAL();
-	const vector< boost::shared_ptr<Vertex> > & getNeighbours(string &id);
-	const map <string , boost::shared_ptr<Vertex> > & getNodes();
+		void addEdge(string &id1  , string& id2);
+		void BFS();
+		void DFS();
+		~GraphAL();
+		const vector < nodeptr >& getNeighbours(string &id);
+		const map <string , nodeptr > & getNodes();
+		void printGraph();
 	private:
-	map< string , boost::shared_ptr<Vertex> > nodes;
+		map< string , nodeptr > nodes;
 };
 
 
